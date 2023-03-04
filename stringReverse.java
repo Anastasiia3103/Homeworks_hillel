dbdf9b1f6d3281454393b02ae546071013307069
Anastasiia3103 <123423011+Anastasiia3103@users.noreply.github.com>
2023-03-04 20:13:47 +0100

    Add files via upload

---
+++ stringReverse.java
@@ -0,0 +1,18 @@
+package hw5;
+
+public class stringReverse {
+
+   public static String reverseRecursively(String source) {
+       if (source == null || source.isEmpty()) {
+           return source;
+       }
+       return source.charAt(source.length() - 1) + reverseRecursively(source.substring(0,source.length() - 1));
+   }
+   
+   
+    public static void main(String [] args){
+        String originalString = "Hello World!";
+        
+        System.out.println(reverseRecursively(originalString));
+    }
+}
