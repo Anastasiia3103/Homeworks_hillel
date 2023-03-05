1db6ac514fd62842d8800dd2fe84da31bdd5fc75
Anastasiia3103 <123423011+Anastasiia3103@users.noreply.github.com>
2023-03-05 13:01:00 +0100

    Add files via upload

---
+++ findSymbolOccurance.java
@@ -0,0 +1,20 @@
+package hw5;
+
+public class findSymbolOccurance {
+    public static int countOccurrences(String str, char c) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main (String[] args) {
+        String str = "Hello, World!";
+        char c = 'o';
+        int count = countOccurrences(str, c);
+        System.out.println("The character '" + c + "' appears " + count + " times in the string.");
+    }
+}
+
---
+++ findWordPosition.java
@@ -0,0 +1,23 @@
+package hw5;
+
+public class findWordPosition {
+
+
+        public static int findWordPosition(String source, String target) {
+            int index = source.indexOf(target);
+            return index;
+        }
+
+        public static void main(String[] args) {
+            String source = "Yellow, black, brown, blue";
+            String target = "brown";
+            int position = findWordPosition(source, target);
+
+            if (position == -1) {
+                System.out.println(target + " not found in " + source);
+            } else {
+                System.out.println(target + " found at position " + position);
+            }
+        }
+    }
+
